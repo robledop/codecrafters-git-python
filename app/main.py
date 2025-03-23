c96fa55a71c997442c5506ccfbb811fc7ee3fbbe
Robledo Pazotto <robledo@gmail.com>
2025-03-23 06:53:58 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
+import datetime
 import enum
 import sys
 import os
 import zlib
 from hashlib import sha1
+import time
+
+GIT_AUTHOR_EMAIL = "robledo@gmail.com"
+GIT_AUTHOR_NAME = "Robledo Pazotto"
 
 
 class Mode(enum.IntEnum):
@@ -35,7 +40,7 @@ def main():
         with open(path, "rb") as f:
             decompressed = zlib.decompress(f.read())
             header, data = decompressed.split(b"\0", maxsplit=1)
-            print(data.decode(encoding="utf-8"), end="")
+            print(data.decode(), end="")
     elif command == "hash-object" and sys.argv[2] == "-w":
         file_name = sys.argv[3]
         sha_hash = hash_file(file_name)
@@ -52,7 +57,7 @@ def main():
                     header, data = data.split(b"\0", maxsplit=1)
                     mode, name = header.split(b" ", maxsplit=1)
                     mode = int(mode)
-                    name = name.decode("utf-8")
+                    name = name.decode()
                     item_hash = data[:20]
 
                     tree_items.append(TreeEntry(mode, name, item_hash))
@@ -76,22 +81,41 @@ def main():
                     print(f"{item.hash.hex()}\t{item.name}")
 
     elif command == "write-tree":
-        current_dir = os.getcwd()
-        tree, tree_hash = hash_directory(current_dir)
-        print(tree, file=sys.stderr)
-        print(tree_hash, end="")
-        folder_path = f".git/objects/{tree_hash[:2]}"
-        hashed_file_name = tree_hash[2:]
-        if not os.path.exists(folder_path):
-            os.mkdir(folder_path)
+        tree = hash_tree(os.getcwd())
+        hashed = sha1(tree).hexdigest()
+        print(hashed, end="")
+        folder_path = f".git/objects/{hashed[:2]}"
+        hashed_file_name = hashed[2:]
+        os.makedirs(folder_path, exist_ok=True)
         with open(f"{folder_path}/{hashed_file_name}", "wb") as hf:
             hf.write(zlib.compress(tree))
+
+    elif command == "commit-tree":
+        tree_sha, p, commit_sha, m, commit_message = sys.argv[2:]
+
+        content = (
+            f"tree {tree_sha}\n"
+            f"parent {commit_sha}\n"
+            f"author {GIT_AUTHOR_NAME} <{GIT_AUTHOR_EMAIL}> {int(time.time())} {time.timezone} "
+            f"commiter {GIT_AUTHOR_NAME} <{GIT_AUTHOR_EMAIL}> {int(time.time())} {time.timezone}\n\n"
+            f"{commit_message}\n"
+        )
+
+        tree = f"commit {len(content)}\0{content}"
+
+        folder_path = f".git/objects/{tree_sha[:2]}"
+        hashed_file_name = tree_sha[2:]
+
+        with open(f"{folder_path}/{hashed_file_name}", "wb") as hf:
+            hf.write(zlib.compress(tree.encode()))
+
+        print(tree_sha, end="")
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
 
-def hash_directory(path):
+def hash_tree(path, write=True):
     dir_items = os.listdir(path)
     git_items = []
     for item in dir_items:
@@ -99,7 +123,7 @@ def hash_directory(path):
             continue
         if os.path.isfile(os.path.join(path, item)):
             print(f"file {item}", file=sys.stderr)
-            file_hash = hash_file(os.path.join(path, item))
+            file_hash = hash_file(os.path.join(path, item), write)
             git_items.append(
                 TreeEntry(
                     Mode.FILE,
@@ -109,8 +133,8 @@ def hash_directory(path):
             )
         elif os.path.isdir(os.path.join(path, item)):
             print(f"directory {item}", file=sys.stderr)
-            _, h = hash_directory(os.path.join(path, item))
-            git_items.append(TreeEntry(Mode.DIRECTORY, item, h))
+            tree = hash_tree(os.path.join(path, item))
+            git_items.append(TreeEntry(Mode.DIRECTORY, item, sha1(tree).hexdigest()))
 
     git_items.sort(key=lambda i: i.name)
 
@@ -121,20 +145,20 @@ def hash_directory(path):
 
     tree = f"tree {str(len(tree_content))}\0".encode() + tree_content
 
-    return tree, sha1(tree).hexdigest()
+    return tree
 
 
-def hash_file(path):
+def hash_file(path, write=True):
     with open(path, "r") as f:
         file_content = f.read()
-        to_be_hashed = f"blob {len(file_content)}\0{file_content}".encode("utf-8")
+        to_be_hashed = f"blob {len(file_content)}\0{file_content}".encode()
         sha_hash = sha1(to_be_hashed).hexdigest()
-        folder_path = f".git/objects/{sha_hash[:2]}"
-        hashed_file_name = sha_hash[2:]
-        if not os.path.exists(folder_path):
-            os.mkdir(folder_path)
-        with open(f"{folder_path}/{hashed_file_name}", "wb") as hf:
-            hf.write(zlib.compress(to_be_hashed))
+        if write:
+            folder_path = f".git/objects/{sha_hash[:2]}"
+            hashed_file_name = sha_hash[2:]
+            os.makedirs(folder_path, exist_ok=True)
+            with open(f"{folder_path}/{hashed_file_name}", "wb") as hf:
+                hf.write(zlib.compress(to_be_hashed))
 
         return sha_hash
 
